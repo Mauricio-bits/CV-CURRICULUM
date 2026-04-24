88f760e8bb6ac34445066967393bb2d085579cf4
Mauricio-bits <mauriciobarboza822@gmail.com>
2026-04-24 13:09:38 -0600

    Update Card.tsx

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,4 +1,10 @@
-export default function Card({ title, description }) {
+// Definimos qué debe recibir esta tarjeta
+interface CardProps {
+  title: string;
+  description: string;
+}
+
+export default function Card({ title, description }: CardProps) {
   return (
     <div className="bg-white shadow-lg rounded-xl p-5 hover:scale-105 transition">
       <h2 className="font-bold text-xl">{title}</h2>
