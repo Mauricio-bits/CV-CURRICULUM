630cc4f3674717f645c283e730c0240fd7b39adc
Mauricio-bits <mauriciobarboza822@gmail.com>
2026-04-24 13:04:18 -0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import "./globals.css";
 import Navbar from "../components/Navbar";
 
-export default function RootLayout({ children }) {
+// Añadimos la definición de tipo para children
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="es">
       <body>
@@ -10,4 +11,4 @@ export default function RootLayout({ children }) {
       </body>
     </html>
   );
-} 
+}
